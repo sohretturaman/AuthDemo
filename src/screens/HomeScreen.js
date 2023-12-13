a1b97dcea0bfcc84917831c79f2062d21bbc11ef
şöhrett <sohretturaman@gmail.com>
2023-12-13 23:07:32 +0300

    corrected homeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,13 +4,14 @@ import { Button, Pressable, StyleSheet, Text, View } from "react-native";
 import React, { useEffect } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { useDispatch, useSelector } from "react-redux";
-import { LoginUser, LogoutUser } from "../store/AuthSlice";
+import { LogoutUser } from "../store/AuthSlice"; // Import LogoutUser action
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const dispatch = useDispatch(); // Import useDispatch from react-redux
 
   const selector = useSelector((state) => state.AuthToken);
-  console.log("selctor value **** new ", selector);
+  console.log("selector value **** new ", selector);
 
   return (
     <View>
@@ -18,7 +19,10 @@ const HomeScreen = () => {
       <Pressable onPress={() => dispatch(LogoutUser())}>
         <Text>Logout </Text>
       </Pressable>
-      <Button title="switch welcome" onPress={navigation.navigate("Welcome")} />
+      <Button
+        title="switch welcome"
+        onPress={() => navigation.navigate("Welcome")}
+      />
     </View>
   );
 };
@@ -26,20 +30,3 @@ const HomeScreen = () => {
 export default HomeScreen;
 
 const styles = StyleSheet.create({});
-
-{
-  /**
- doğru kod örneği asyn için,
-
-   const dispatch = useDispatch();
-
-  useEffect(() => {
-    const handleSavedUser = () => {
-      dispatch(LoginUser(tokenId)); //you can take data from here too
-    };
-    // call the function
-    handleSavedUser();
-  }, []);
-
-*/
-}
